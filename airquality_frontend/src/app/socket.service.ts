a72e34565eec414ff917ca7c9f37ec89243405e3
michael.jeremias <michael.jeremias.home@gmail.com>
2023-04-18 10:43:55 +0200

    socket url and some gui stuff

--- airquality_frontend/src/app/socket.service.ts
+++ airquality_frontend/src/app/socket.service.ts
@@ -3,37 +3,37 @@ import * as socketIO from "socket.io-client";
 import { Observable, fromEvent, map } from "rxjs";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class SocketService {
-  private socket;
+    private socket;
 
-  constructor() {
-    this.socket = socketIO.io("ws://127.0.0.1:5000");
-    // this.socket = socketIO.io("ws://localhost:5000");
-  }
+    constructor() {
+        this.socket = socketIO.io("ws://raspberrypi:5000");
+        // this.socket = socketIO.io("ws://localhost:5000");
+    }
 
-  sendMessage(message: string) {
-    this.socket.emit("message", message);
-  }
+    sendMessage(message: string) {
+        this.socket.emit("message", message);
+    }
 
-  onMessage() {
-    return fromEvent(this.socket, "message");
-  }
+    onMessage() {
+        return fromEvent(this.socket, "message");
+    }
 
-  onData(): Observable<SensorData[]> {
-    return fromEvent(this.socket, "on_data").pipe(map((value: string) => JSON.parse(value)));
-  }
+    onData(): Observable<SensorData[]> {
+        return fromEvent(this.socket, "on_data").pipe(map((value: string) => JSON.parse(value)));
+    }
 
-  isConnected(): boolean {
-    console.log(this.socket);
-    return this.socket.connected;
-  }
+    isConnected(): boolean {
+        console.log(this.socket);
+        return this.socket.connected;
+    }
 }
 
 export interface SensorData {
-  name: string;
-  subject: string;
-  unit: string;
-  value: number;
+    name: string;
+    subject: string;
+    unit: string;
+    value: number;
 }
